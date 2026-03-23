3aac989df593bd016f73c6728f535fb6e349b714
Shayan Sheikh <shayan.faisal1095@gmail.com>
2026-03-23 03:31:20 -0400

    new changes again

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,12 +3,41 @@
 import './App.css'
 import { languages } from './languages'
 import {nanoid} from 'nanoid' 
+import { useState } from 'react'
+import { clsx } from 'clsx';
 
 function App() {
 
-  const displayArray = languages.map((langauge) => { return <span key={nanoid()}  style={{ backgroundColor : langauge.backgroundColor, color : langauge.color}}   className='langBlock'>{langauge.name}</span>})
+  const [Word, setWord] = useState("REACT")
+  const [letters, setLetters] = useState([])
+  const arrWord = Word.split("")
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const arrAlphabet = alphabet.split("")
+  
 
-  return (
+  function handleKeyboardeClick(letter){
+   
+
+    setLetters(prevLetter => {
+        const letterSet = new Set(prevLetter)
+        letterSet.add(letter)
+        return Array.from(letterSet)
+    }
+// using a set to make sure there are no duplicate letters in the letter array
+    )
+
+  }
+
+  const displayArray = languages.map((langauge) => { return <span key={nanoid()}  style={{ backgroundColor : langauge.backgroundColor, color : langauge.color}}   className='langBlock'>{langauge.name}</span>})
+  const displayArrWord = arrWord.map((letter) =>(<span key={nanoid()}  className='letterBox'>{letter}</span> ))
+  const  displayArrAlphabet = arrAlphabet.map((letter) =>(<button key={nanoid()} onClick={() => handleKeyboardeClick(letter)}  className={clsx('alphaButton',{
+     correct: letters.includes(letter) && arrWord.includes(letter.toUpperCase()),
+    incorrect: letters.includes(letter) && !arrWord.includes(letter.toUpperCase())
+  })}>{letter}</button>))
+ 
+ 
+ 
+  return (  
     <>
 
            <header className="title">
@@ -26,6 +55,9 @@ function App() {
 
 
            <div className='langContainer'>{displayArray}</div>
+           <div className="wordContainer">{displayArrWord}</div>
+           <div className='keyBoard'>{displayArrAlphabet}</div>
+           <div className='newGame'><button>New Game</button></div>
      
     </>
   )
